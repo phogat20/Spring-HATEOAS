dd0da56d9612f3e0711d7c3a549ecc931dba80ad
CHETAN SINGH PHOGAT <84695818+phogat20@users.noreply.github.com>
2023-04-26 07:36:08 +0530

    Update HATEOAS-Controller.java

--- HATEOAS-Controller.java
+++ HATEOAS-Controller.java
@@ -8,8 +8,16 @@
 		return entityModel;
 	}
  
-	// HATEOAS concepts-
-	// - EntityModel
-	// - WebMVCLinkBuilder
+// 	# HATEOAS concepts-
+
+// 	1. EntityModel
+//       - EntityModel is a class in Spring HATEOAS that represents a resource with hypermedia links. 
+//       - It is similar to the RepresentationModel class, but it is specifically designed to wrap 
+//	   domain objects and provide hypermedia links.
+      
+// 	2. WebMVCLinkBuilder
+//       - WebMvcLinkBuilder is a utility class in Spring HATEOAS that provides methods for
+//           building hypermedia links in a Spring MVC environment. 
+//       - It can be used to create links to controller methods, static resources, or external URLs.
                                     
                                     
